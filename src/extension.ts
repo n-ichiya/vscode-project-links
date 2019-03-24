fc7ed1f3174aec2697f65c496d04f8d819173997
None <non@none.com>
2019-03-24 11:10:36 +0000

    Add relative paths

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,4 @@
-"use strict";
+'use strict'
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import {
@@ -12,17 +12,18 @@ import {
   Range,
   Position,
   Uri
-} from "vscode";
+} from 'vscode'
+
+import { join } from 'path';
 
 // import { stat, Stats } from "fs";
 
-const MAX_LENGTH = 100000;
+const MAX_LENGTH = 100000
 
-const regex = /project:\/\/([^\s]+)/;
-const regexGlobal = new RegExp(regex, regex.flags + "g");
+const regex = /project:\/\/([^\s]+)/
+const regexGlobal = new RegExp(regex, regex.flags + 'g')
 
-type TargetedDocumentLink = DocumentLink & { target: Uri };
-
+type TargetedDocumentLink = DocumentLink & { target: Uri }
 
 // const enableFiltering = false;
 
@@ -47,55 +48,68 @@ class LinkProvider implements DocumentLinkProvider {
     document: TextDocument,
     token: CancellationToken
   ): undefined | Thenable<DocumentLink[]> | DocumentLink[] {
-    const workspaceFolder = workspace.getWorkspaceFolder(document.uri);
-    if (!workspaceFolder || workspaceFolder.uri.scheme !== "file")
-      return undefined;
+    const workspaceFolder = workspace.getWorkspaceFolder(document.uri)
+    if (!workspaceFolder || workspaceFolder.uri.scheme !== 'file')
+      return undefined
     const str = document.getText(
       new Range(new Position(0, 0), document.positionAt(MAX_LENGTH))
-    );
+    )
 
-    regexGlobal.lastIndex = 0;
-    const links: TargetedDocumentLink[] = [];
+    regexGlobal.lastIndex = 0
+    const links: TargetedDocumentLink[] = []
     for (
       let match = regexGlobal.exec(str);
       match;
       match = regexGlobal.exec(str)
     ) {
-      try{
-        const url = Uri.parse( match[0].replace(/\/\//,"///"));      
-        links.push({
-          range: new Range(
-            document.positionAt(match.index),
-            document.positionAt(match.index + match[0].length)
-          ),
-          target: workspaceFolder.uri.with({
-            path: workspaceFolder.uri.path + url.path,
-            query: url.query,
-            fragment: url.fragment
+      try {
+        const url = Uri.parse(match[0].replace(/\/\//, '///'))
+        // First path element is relative .
+        if (/[.]{1,2}/g.test(url.path)) {
+          let fpath = document.uri.path.split('/')
+          fpath.splice(fpath.length-1);
+          links.push({
+            range: new Range(
+              document.positionAt(match.index),
+              document.positionAt(match.index + match[0].length)
+            ),
+            target: document.uri.with({
+              path: join(...fpath, url.path),
+              query: url.query,
+              fragment: url.fragment
+            })
           })
-        });
-
-      }catch(e){
+        } else {
+          links.push({
+            range: new Range(
+              document.positionAt(match.index),
+              document.positionAt(match.index + match[0].length)
+            ),
+            target: workspaceFolder.uri.with({
+              path: workspaceFolder.uri.path + url.path,
+              query: url.query,
+              fragment: url.fragment
+            })
+          })
+        }
+      } catch (e) {
         //link is somehow invalid
       }
     }
-    return links;
+    return links
   }
 
   resolveDocumentLink(link: DocumentLink, token: CancellationToken) {
-    return undefined;
+    return undefined
   }
 }
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
-  const lp = new LinkProvider();
+  const lp = new LinkProvider()
   context.subscriptions.push(
-    languages.registerDocumentLinkProvider(
-      {scheme: "file"},
-      lp
-    )
-  );
+    languages.registerDocumentLinkProvider({ scheme: 'file' }, lp)
+  )
 }
 
 // this method is called when your extension is deactivated
